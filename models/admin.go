e3e2055fcb3e61241ce49a04bc52319d6d5c4de5
scarlet-arch <phantom190514@gmail.com>
2024-10-19 00:04:58 +0800

    B[FEATURE] add model api

--- models/admin.go
+++ models/admin.go
@@ -1,10 +1,43 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"gorm.io/gorm"
+
+	log "github.com/sirupsen/logrus"
+)
 
 type Admin struct {
 	gorm.Model
 	AdminName string
 	Account   string
 	Password  string
+}
+
+//TODO: 添加单元测试(cyt on 2024-10-19)
+func ExistAdmin(account string) (bool, error) {
+	var a Admin
+	err := db.Model(&Admin{}).Where("account = ?", account).First(&a).Error
+	return a.ID > 0, err
+}
+
+func GetAdmin(account string) (a *Admin, err error) {
+	err = db.Model(&Admin{}).Where("account = ?", account).First(a).Error
+	return
+}
+
+func EditAdmin(account string, data any) error {
+	err := db.Model(&Admin{}).Where("account = ?", account).Updates(data).Error
+	return err
+}
+
+func CleanAllAdmin() error {
+	log.Info("running admin cleaning")
+	defer log.Info("Deleted admins have been cleaned")
+	err := db.Unscoped().Where("deleted_at IS NOT NULL").Delete(&Admin{}).Error
+	return err
+}
+
+func DeleteAdmin(account string) error {
+	err := db.Where("account = ?", account).Delete(&Admin{}).Error
+	return err
 }
--- scripts/setup.sql
+++ scripts/setup.sql
@@ -16,7 +16,9 @@ create table admin(
   `updated_at` datetime not null,
   `deleted_at` datetime,
   `admin_name` varchar(50) not null,
-  `account` varchar(100) not null,
+  `account` varchar(100) not null unique,
   `password` varchar(50) not null,
   primary key(id)
 ) engine=innodb default charset=utf8 comment='管理员账户';
+-- 为account创建索引
+CREATE INDEX idx_account ON admin(account);
