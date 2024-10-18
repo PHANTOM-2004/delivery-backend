7a9f58f1f2ae376f7bf7e086bc4818650c17b067
scarlet-arch <phantom190514@gmail.com>
2024-10-19 00:11:37 +0800

    B[FIX] optimize model logic

--- models/admin.go
+++ models/admin.go
@@ -1,6 +1,8 @@
 package models
 
 import (
+	"errors"
+
 	"gorm.io/gorm"
 
 	log "github.com/sirupsen/logrus"
@@ -13,16 +15,20 @@ type Admin struct {
 	Password  string
 }
 
-//TODO: 添加单元测试(cyt on 2024-10-19)
+// TODO: 添加单元测试(cyt on 2024-10-19)
 func ExistAdmin(account string) (bool, error) {
 	var a Admin
 	err := db.Model(&Admin{}).Where("account = ?", account).First(&a).Error
+	if errors.Is(err, gorm.ErrRecordNotFound) {
+		return false, nil
+	}
 	return a.ID > 0, err
 }
 
-func GetAdmin(account string) (a *Admin, err error) {
-	err = db.Model(&Admin{}).Where("account = ?", account).First(a).Error
-	return
+func GetAdmin(account string) (*Admin, error) {
+	a := &Admin{}
+	err := db.Model(&Admin{}).Where("account = ?", account).First(&a).Error
+	return a, err
 }
 
 func EditAdmin(account string, data any) error {
@@ -33,7 +39,9 @@ func EditAdmin(account string, data any) error {
 func CleanAllAdmin() error {
 	log.Info("running admin cleaning")
 	defer log.Info("Deleted admins have been cleaned")
-	err := db.Unscoped().Where("deleted_at IS NOT NULL").Delete(&Admin{}).Error
+	res := db.Unscoped().Where("deleted_at IS NOT NULL").Delete(&Admin{})
+	err := res.Error
+	log.Infof("rows affected: [%d]", res.RowsAffected)
 	return err
 }
 
@@ -41,3 +49,5 @@ func DeleteAdmin(account string) error {
 	err := db.Where("account = ?", account).Delete(&Admin{}).Error
 	return err
 }
+
+
