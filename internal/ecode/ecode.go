8158b1715f8a2f97644450aa4dcaac41a9ce317c
rick-arch <phantom190514@gmail.com>
2024-10-19 08:15:37 +0800

    B[FEATURE] add global error code

---
+++ internal/ecode/ecode.go
@@ -0,0 +1,42 @@
+package ecode
+
+import (
+	"net/http"
+
+	log "github.com/sirupsen/logrus"
+)
+
+type Ecode = int
+
+const (
+	SUCCESS        Ecode = http.StatusOK
+	ERROR          Ecode = http.StatusInternalServerError
+	INVALID_PARAMS Ecode = 1001
+
+	ERROR_AUTH_CHECK_TOKEN_FAIL    Ecode = 20001
+	ERROR_AUTH_CHECK_TOKEN_TIMEOUT Ecode = 20002
+	ERROR_AUTH_TOKEN               Ecode = 20003
+	ERROR_AUTH                     Ecode = 20004
+)
+
+func StatusText(e Ecode) (res string) {
+	switch e {
+	case SUCCESS:
+		res = "ok"
+	case ERROR:
+		res = "fail"
+	case INVALID_PARAMS:
+		res = "请求参数错误"
+	case ERROR_AUTH_CHECK_TOKEN_FAIL:
+		res = "Token鉴权失败"
+	case ERROR_AUTH_CHECK_TOKEN_TIMEOUT:
+		res = "Token已超时"
+	case ERROR_AUTH_TOKEN:
+		res = "Token生成失败"
+	case ERROR_AUTH:
+		res = "Token错误"
+	default:
+		log.Fatalf("未知错误码[%d]", e)
+	}
+	return
+}
