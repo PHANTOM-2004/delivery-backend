41821d76df9a65c81517b2c81924dadce853aafb
scarlet-arch <phantom190514@gmail.com>
2024-11-03 15:36:56 +0800

    B[FEATURE] add random string generator

--- pkg/utils/common.go
+++ pkg/utils/common.go
@@ -3,8 +3,28 @@ package utils
 import (
 	"errors"
 	"fmt"
+	"math/rand"
 	"reflect"
+	"strings"
+	"time"
 )
+
+const letters = "abcdefghijklmnopqrstuvwxyz0123456789"
+
+func RandString(n int) string {
+	r_src := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(r_src)
+
+	var sb strings.Builder
+
+	for i := 0; i < n; i++ {
+		pos := r.Intn(len(letters))
+		s := string(letters[pos])
+		sb.WriteString(s)
+	}
+
+	return sb.String()
+}
 
 func StructToStr(s any) (string, error) {
 	v := reflect.ValueOf(s).Elem()
