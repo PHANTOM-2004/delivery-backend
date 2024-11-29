8e412eb46e3ab0932ef354394c4ecb40fa39cd37
scarlet-arch <phantom190514@gmail.com>
2024-11-29 15:57:07 +0800

    B[FEATURE] add addressbook model

---
+++ routers/api/v1/wechat_address.go
@@ -0,0 +1,18 @@
+package v1
+
+import "github.com/gin-gonic/gin"
+
+func UpdateAddressBook(c *gin.Context) {
+}
+
+func SetDefaultAddressBook(c *gin.Context) {
+}
+
+func CreateAddressBook(c *gin.Context) {
+}
+
+func DeleteAddressBook(c *gin.Context) {
+}
+
+func GetAddressBook(c *gin.Context) {
+}
