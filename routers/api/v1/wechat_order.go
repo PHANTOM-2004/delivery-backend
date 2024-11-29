f2c1b6d1d3cf4b5343aafd117b9455eb783b86aa
scarlet-arch <phantom190514@gmail.com>
2024-11-30 00:49:50 +0800

    B[FEATURE] add create order api

--- routers/api/v1/wechat_order.go
+++ routers/api/v1/wechat_order.go
@@ -5,11 +5,19 @@ import (
 	"delivery-backend/internal/ecode"
 	"delivery-backend/middleware/wechat"
 	"delivery-backend/models"
+	wechat_service "delivery-backend/service/wechat"
 	"net/http"
 	"strconv"
 
 	"github.com/gin-gonic/gin"
+	log "github.com/sirupsen/logrus"
 )
+
+type CreateOrderRequest struct {
+	Address      string `json:"address" validate:"max=100"`
+	CustomerName string `json:"customer" validate:"max=20"`
+	PhoneNumber  string `json:"phone_number" validate:"e164"`
+}
 
 func CreateOrder(c *gin.Context) {
 	restaurant_id, err := strconv.Atoi(c.Param("restaurant_id"))
@@ -17,7 +25,24 @@ func CreateOrder(c *gin.Context) {
 		app.ResponseInvalidParams(c)
 		return
 	}
+	/////////////////////////////////////
+	// parse请求
+	var req CreateOrderRequest
+	err = c.ShouldBindJSON(&req)
+	if err != nil {
+		app.ResponseInvalidParams(c)
+		log.Debug(err)
+		return
+	}
+	err = app.ValidateStruct(&req)
+	if err != nil {
+		app.ResponseInvalidParams(c)
+		log.Debug(err)
+		return
+	}
 
+	/////////////////////////////////////
+	//从session中验证购物车
 	session := wechat.DefaultSession(c)
 	cart, err := session.GetCart(uint(restaurant_id))
 	if err != nil {
@@ -29,20 +54,50 @@ func CreateOrder(c *gin.Context) {
 		app.Response(c, http.StatusOK, ecode.ERROR_WX_CART_EMPTY, nil)
 		return
 	}
+	info, err := session.GetInfo()
+	if err != nil {
+		app.ResponseInternalError(c, err)
+		return
+	}
 
-	order := models.Order{}
-	// TODO:
+	/////////////////////////////////////
+	// NOTE:
 	// 0.生成PickupNo
 	// 1.生成OrderNo
 	// 2.填入Address (from json)
 	// 3.填入CustomerName (from json)
 	// 4.绑定WechatUserID (from session)
 	// 5.填入PhoneNumber(from json)
+	order_gen := wechat_service.NewOrderGen(uint(restaurant_id))
+	var order_no, pickup_number string
+	pickup_number, err = order_gen.GetPickupNo()
+	if err == nil {
+		order_no, err = order_gen.GetOrderNo()
+	}
+	if err != nil {
+		// 下单失败
+		app.Response(c,
+			http.StatusOK,
+			ecode.ERROR_WX_ORDER_CREATE, nil)
+		return
+	}
 
+	/////////////////////////////////////
+	// 成功生成pickup no, order no
+	order := models.Order{
+		PickupNo:     pickup_number,
+		OrderNo:      order_no,
+		Address:      req.Address,
+		PhoneNumber:  req.PhoneNumber,
+		CustomerName: req.CustomerName,
+		WechatUserID: info.ID,
+	}
 	err = models.CreateOrder(uint(restaurant_id), &order, cart)
 	if err != nil {
 		// 下单失败
-		app.Response(c, http.StatusOK, ecode.ERROR_WX_ORDER_CREATE, nil)
+		app.Response(c,
+			http.StatusOK,
+			ecode.ERROR_WX_ORDER_CREATE, nil)
 		return
 	}
 	// 首先create order, 其次create order details
--- service/wechat/pickup_number.go
+++ service/wechat/pickup_number.go
@@ -3,24 +3,45 @@ package wechat_service
 import (
 	"delivery-backend/internal/gredis"
 	"fmt"
+	"strconv"
+	"time"
 )
 
 // TODO:采用定时任务，向数据库中刷新PickUpNumber
 // 以及每天定时清零
-type PickupNo struct {
+type OrderGen struct {
 	restaurant_id uint
 }
 
-func NewPickupNo(restaurant_id uint) *PickupNo {
-	return &PickupNo{restaurant_id: restaurant_id}
+func NewOrderGen(restaurant_id uint) *OrderGen {
+	return &OrderGen{restaurant_id: restaurant_id}
 }
 
-func (p *PickupNo) key() string {
+func (p *OrderGen) counterKey() string {
 	key := fmt.Sprintf("restaurant[%d]order_counter", p.restaurant_id)
 	return key
 }
 
-func (p *PickupNo) Get(restaurant_id uint) (int, error) {
-	num, err := gredis.Incre(p.key())
-	return int(num), err
+func (p *OrderGen) numberKey() string {
+	key := fmt.Sprintf("restaurant[%d]order_number", p.restaurant_id)
+	return key
+}
+
+func (p *OrderGen) GetPickupNo() (string, error) {
+	num, err := gredis.Incre(p.counterKey())
+	return strconv.Itoa(int(num)), err
+}
+
+func (p *OrderGen) GetOrderNo() (string, error) {
+	num, err := gredis.Incre(p.counterKey())
+	if err != nil {
+		return "", err
+	}
+	currentTime := time.Now()
+	formattedTime := currentTime.Format("20060102150405")
+	res := fmt.Sprintf("%s%d%d",
+		formattedTime,
+		190514*p.restaurant_id,
+		num)
+	return res, nil
 }
