0f80c8c8114e37dda4f462d88a134905f17285d7
rick-arch <phantom190514@gmail.com>
2024-12-03 20:57:41 +0800

    B[FIX] fix data sciprts

--- scripts/dish_data.sh
+++ scripts/dish_data.sh
@@ -2,11 +2,14 @@
 
 set -x
 
+# 利用convert做一个假图片
+convert -size 32x32 xc:white empty.jpg
+
 # 向餐厅1插入dish 1
 curl -b cookies.txt \
   -F "name=雪豹炒鸡扒" \
   -F "price=2089" \
-  -F "image=@Makefile.png" \
+  -F "image=@empty.jpg" \
   -F "description=好的" \
   http://localhost:8000/api/v1/merchant/restaurant/1/dish \
   --request POST
@@ -15,7 +18,7 @@ curl -b cookies.txt \
 curl -b cookies.txt \
   -F "name=西瓜炖土豆" \
   -F "price=1999" \
-  -F "image=@Makefile.png" \
+  -F "image=@empty.jpg" \
   -F "description=你知道我要说什么" \
   http://localhost:8000/api/v1/merchant/restaurant/1/dish \
   --request POST
