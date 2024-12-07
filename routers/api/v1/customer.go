a4b8d5db14f87defb828dd5f022a70a369f08eed
rick-arch <phantom190514@gmail.com>
2024-12-07 23:10:40 +0800

    B[FIX] fix merchant application to adjust wechat json api

--- routers/api/v1/customer.go
+++ routers/api/v1/customer.go
@@ -2,81 +2,51 @@ package v1
 
 import (
 	"delivery-backend/internal/app"
-	"delivery-backend/internal/ecode"
-	"delivery-backend/internal/setting"
 	"delivery-backend/models"
-	"delivery-backend/service/customer_service"
-	"net/http"
 	"strconv"
 
 	"github.com/gin-gonic/gin"
 	log "github.com/sirupsen/logrus"
 )
 
+type MerchantApplyReq struct {
+	Description string `json:"description" validate:"min=1,max=300"`
+	Email       string `json:"email" validate:"required,email"`
+	PhoneNumber string `json:"phone_number" validate:"required,e164"`
+	Name        string `json:"name" validate:"min=2,max=20"`
+	Path        string `json:"path" validate:"max=80"`
+}
+
 // 由顾客/骑手发起申请，提出商务合作请求
 // https://gin-gonic.com/docs/examples/upload-file/single-file/
 func MerchantApply(c *gin.Context) {
-	// TODO: 顾客身份校验
-
-	file, err := c.FormFile("license")
+	req := MerchantApplyReq{}
+	err := c.ShouldBindJSON(&req)
 	if err != nil {
 		log.Debug(err)
 		app.ResponseInvalidParams(c)
 		return
 	}
-	ext, v := setting.AppSetting.CheckLicenseExt(file.Filename)
-	if !v {
-		log.Debugf("wrong ext[%s]", ext)
-		app.ResponseInvalidParams(c)
-		return
-	}
-
-	a := customer_service.Application{
-		Description: c.PostForm("description"),
-		Email:       c.PostForm("email"),
-		PhoneNumber: c.PostForm("phone_number"),
-		Name:        c.PostForm("name"),
-	}
-
-	err = app.ValidateStruct(&a)
+	err = app.ValidateStruct(&req)
 	if err != nil {
 		log.Debug(err)
 		app.ResponseInvalidParams(c)
 		return
 	}
-
-	log.Debug("uploadint: ", file.Filename)
-	// 保存证书，并且使用id重命名,更改回ext
-	name := setting.AppSetting.GenLicenseName() + ext
-	dst := setting.AppSetting.GetLicenseStorePath(name)
-	err = c.SaveUploadedFile(file, dst)
-	if err != nil {
-		app.ResponseInternalError(c, err)
-		return
-	}
-	log.Trace("saved to:", dst)
-
-	// 插入数据库
-
 	data := models.MerchantApplication{
-		Description: a.Description,
-		License:     name,
-		Email:       a.Email,
-		PhoneNumber: a.PhoneNumber,
-		Name:        a.Name,
+		Description: req.Description,
+		License:     req.Path,
+		Email:       req.Email,
+		PhoneNumber: req.PhoneNumber,
+		Name:        req.Name,
 	}
 
 	err = models.CreateMerchantApplication(&data)
 	if err != nil {
-		log.Warn(err)
-		app.Response(c, http.StatusInternalServerError, ecode.ERROR, nil)
+		app.ResponseInternalError(c, err)
 		return
 	}
-
-	// 成功保存
-	log.Debugf("license[%s] saved to %s", file.Filename, dst)
-
-	app.Response(c, http.StatusOK, ecode.SUCCESS, nil)
+	app.ResponseSuccess(c)
 }
 
 func GetRestaurantCategoryDish(c *gin.Context) {
